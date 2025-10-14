ba8c9b12f5a0ed4749647b3eccbc54ba0f79e6d8
TrinaBee <der3ndgegner@gmail.com>
2025-10-14 11:58:28 +0200

    V1.0

--- Datentypen/aufgabe3-familie-maier.php
+++ Datentypen/aufgabe3-familie-maier.php
@@ -8,7 +8,6 @@
 
 <?php
 
-// Beispiel einer einfachen Klasse
 class Auto {
     public $marke;
     public $modell;
@@ -25,38 +24,50 @@ class Auto {
     }
 
     public function ausgeben() {
-        echo "Auto: $this->marke $this->modell, BJ: $this->baujahr, Farbe: $this->farbe, PS:$this->ps";
+        return "Auto: $this->marke $this->modell, BJ: $this->baujahr, Farbe: $this->farbe, PS:$this->ps";
     }
 }
 
-// Erstellen eines Objekts
-//$meinAuto = new Auto("Tesla", "Model S");
-//$meinAuto->ausgeben();  // Ausgabe: Auto: Tesla Model S
+
 $maier = [
-        "Vater Alfred" => [ new Auto("Audi","A4","2023","schwarz","180")],
-        "Mutter Ingrid" => [ new Auto("VW","Käfer","2019","gelb","110")],
-        "Tochter Lisa" => [ new Auto("VW","up!","2025","rot","90")]
+        "Vater Alfred" => [["Audi","A4","2023","schwarz","180"]],
+        "Mutter Ingrid" => [["VW","Käfer","2019","gelb","110"]],
+        "Tochter Lisa" => [ ["VW","up!","2025","rot","90"]]
 ];
 echo "<pre>";
-//print_r($maier);
+print_r($maier);
 echo "</pre>";
 echo "<hr>";
-//foreach ($maier as $mitglied => $fahrzeug){
-//    echo "<ul><li>$mitglied</li><ul>";
-//    echo "<li>$fahrzeug->ausgeben()</li>";
-//    }
-//    echo "</ul></ul>";
-//}
-//echo "<hr>";
+foreach ($maier as $mitglied => $flotte){
+    echo "<ul><li>$mitglied</li><ul>";
+    foreach($flotte as $fahrzeug){
+    list($marke,$modell,$baujahr,$farbe,$ps) = $fahrzeug;
+    $neues_auto = new Auto($marke,$modell,$baujahr,$farbe,$ps);
 
-$maier["Vater Alfred2"] = [ new Auto("Porsche","unbekannt","2025","weiß","460")];
-//foreach ($maier as $mitglied => $fahrzeug){
-//    echo "<ul><li>$mitglied</li><ul>";
-//    foreach ($fahrzeug as $key => $value){
-//        echo "<li>$key : $value</li>";
-//    }
-//    echo "</ul></ul>";
-//}
+    echo "<li>" . $neues_auto->ausgeben() . "</li>";
+    }
+    echo "</ul></ul>";
+}
+echo "<hr>";
+
+array_push($maier["Vater Alfred"], ["Porsche","unbekannt","2025","weiß","460"]);
+
+echo "<pre>";
+print_r($maier);
+echo "</pre>";
+echo "<hr>";
+
+foreach ($maier as $mitglied => $flotte){
+    echo "<ul><li>$mitglied</li><ul>";
+    foreach($flotte as $fahrzeug){
+        list($marke,$modell,$baujahr,$farbe,$ps) = $fahrzeug;
+        $neues_auto = new Auto($marke,$modell,$baujahr,$farbe,$ps);
+
+        echo "<li>" . $neues_auto->ausgeben() . "</li>";
+    }
+    echo "</ul></ul>";
+}
+echo "<hr>";
 ?>
 
 
